b6fa0fcf70b17ae08661cca5d8a1e613bc103070
Yanika Bugeja <yanika.bugeja@haud.com>
2022-01-27 10:30:20 +0100

    Included .ENV File + Compiled Using Truffle Compile

--- truffle.js
+++ truffle.js
@@ -1,11 +1,32 @@
+require('dotenv').config()
+
+const path = require('path');
+const mnemonic = process.env.MNEMONIC_PASSWORD;
+const RinkebyWSEndPoint = process.env.RINKEBY_WS_END_POINT;
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // for more about customizing your Truffle configuration!
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "*" // Match any network id
+      network_id: "*",
+      gasPrice: 10000000000,
+      gas: 6721975
+    },
+    rinkeby: {
+      provider: () => {
+        return new HDWalletProvider(mnemonic, RinkebyWSEndPoint)
+      },
+      network_id: 4,
+      gasPrice: 10000000000,
+      gas: 4500000
+    }
+  },
+  compilers: {
+    solc: {
+      version: '0.8.0',
+      parser: 'solcjs'
     }
   }
 };
