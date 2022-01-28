69d10042090abd61a07b55c85caadb45bc77b0b9
Yanika Bugeja <yanika.bugeja@haud.com>
2022-01-28 07:50:51 +0100

    Updated Unit Testing

--- test/xman_test.js
+++ test/xman_test.js
@@ -46,29 +46,14 @@ contracts('XManBusinessLogic', (accounts) => {
     )
   })
 
-  it('Transferring XMan Tokens - Should send 10 XMan tokens', async () => {
-    var fristAccount = accounts[0];
-    var secondAccount = accounts[1];
-    var fristAccountStartBalance = await contractInstance.balanceOf.call(accounts[0]);
-    var secondAccountStartBalance = await contractInstance.balanceOf.call(accounts[1]);
+  it('Returning XMan - Should return the identified', async () => {
+    const GAS_LIMIT = 500000;
 
-    await contractInstance.transfer(secondAccount, 10, {from: fristAccount})
-
-    var fristAccountFinalBalance = await contractInstance.balanceOf.call(fristAccount);
-    var secondAccountFinalBalance = await contractInstance.balanceOf.call(secondAccount);
-    var firstAccountBalance = fristAccountStartBalance - 10;
-    var secondAccountBalance = secondAccountStartBalance + 10;
-
+    let XManQuery = await contractInstance.getXManCount({from: account[0], gas: GAS_LIMIT});
     assert.equals(
-      fristAccountFinalBalance.toNumber(),
-      firstAccountBalance,
-      'XMan Tokens Transfer Failed From Sender'
-    )
-
-    assert.equals(
-      secondAccountFinalBalance.toNumber(),
-      secondAccountBalance,
-      'XMan Tokens Transfer Failed From Receiver'
+      XManQuery,
+      0,
+      'Xman Not Identified'
     )
   })
 
@@ -79,7 +64,22 @@ contracts('XManBusinessLogic', (accounts) => {
     assert.equals(
       XManQuery,
       0,
-      'Xman Not Identified'
+      'XMan Not Identified'
+    )
+  })
+
+  it('Creating XMan - Should create the XMan', async () => {
+    const GAS_LIMIT = 500000;
+
+    let XManQuery  = await contractInstance.createRandomXMan("XMan Creation", 100000000, {
+      from: account[0],
+      gas: GASS_LIMIT
+    })
+
+    assert.equals(
+      XManQuery,
+      0,
+      'XMan Not Created'
     )
   })
 })
